010a12e66565bee33565fbb254e193dabed8220a
Federico Libonatu <flibonati@mail.austral.edu.ar>
2026-04-06 12:09:05 -0300

    Add files via upload

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,59 @@
 # Replace the "ANSWER HERE" for your answer
 
 def roots(a, b, c):
-    return "ANSWER HERE"
+    discriminante = b ** 2 - 4 * a * c
 
+    if discriminante < 0:
+        return "( )"
+    elif discriminante == 0:
+        r = (-b) / (2 * a)
+        return f"({r})"
+    else:
+        raiz_disc = discriminante ** 0.5
+        r1 = (-b + raiz_disc) / (2 * a)
+        r2 = (-b - raiz_disc) / (2 * a)
+        return f"({r1}, {r2})"
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
-
+    return a * x ** 2 + b* x + c
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
+    resultado = "f(x) = "
+
+    if a != 0:
+        resultado += f"{a} * X^2"
+
+    if b != 0:
+        if resultado != "f(x) = ":
+            resultado += " + "
+        resultado += f"{b} * X"
+
+    if c != 0:
+        if resultado != "f(x) = ":
+            resultado += " + "
+        resultado += f"{c}"
+
+    if resultado == "f(x) = ":
+        resultado += "0"
+
+    return resultado
 
 
 def derivation(a, b, c):
-    return "ANSWER HERE"
+    resultado = "f'(x) = "
+
+    coef = 2 * a
+
+    if coef != 0:
+        resultado += f"{coef} * X"
+
+    if b != 0:
+        if resultado != "f'(x) = ":
+            resultado += " + "
+        resultado += f"{b}"
+
+    # Si todo es 0
+    if resultado == "f'(x) = ":
+        resultado += "0"
+
+    return resultado
--- text_analyzer.py
+++ text_analyzer.py
@@ -23,7 +23,10 @@ def total_letters(text):
     Retorna la cantidad total de letras en el texto.
     Debe USAR las funciones count_vowels y count_consonants.
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    vocals= int(count_vowels(text))
+    consonantes= int(count_consonants(text))
+    total= vocals + consonantes
+    return total
 
 def vowel_percentage(text):
     """
@@ -33,7 +36,15 @@ def vowel_percentage(text):
 
     Ejemplo: "hola" tiene 2 vocales de 4 letras → 50.0
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    total= (total_letters(text))
+    vocales = (count_vowels(text))
+
+
+    if total == 0:
+        return 0.0
+
+    porcentaje = (vocales / total) * 100
+    return round(porcentaje,1)
 
 def analyze_text(text):
     """
@@ -44,4 +55,9 @@ def analyze_text(text):
 
     Ejemplo: analyze_text("hola") → "V:2 C:2 T:4 P:50.0%"
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    vowels= count_vowels(text)
+    consonants= count_consonants(text)
+    total= total_letters(text)
+    percentage= vowel_percentage(text)
+
+    return f'V:{vowels} C:{consonants} T:{total} P:{percentage}%'
